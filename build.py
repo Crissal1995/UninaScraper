b5fe37873e1836df7539775596a1d7039e5305bd
Cristiano <cristianosalerno1995@gmail.com>
2019-10-15 16:17:36 +0200

    Aggiunto shebang sia a build che a scraper

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 try:
   import python_minifier as mini
   has_mini = True
@@ -7,6 +8,8 @@ import os
 
 files = ['unina_scraper_model.py', 'unina_scraper.py']
 outfile = 'scraper.py'
+lines_first = ["#!/usr/bin/env python3\n"]
+lines_after = []
 forbidden_lines = ['from unina_scraper_model import *\n']
 
 # clean outfile if it already exists
@@ -17,15 +20,19 @@ except FileNotFoundError:
   pass
 
 with open(outfile, 'a') as out:
+  out.writelines(lines_first)
   for f in files:
     fh = open(f)
     ls = fh.readlines()
     out.writelines([l for l in ls if l not in forbidden_lines])
     fh.close()
+  out.writelines(lines_after)
 
 if has_mini:
   with open(outfile) as out:
     content = out.read()
 
   with open(outfile, 'w') as out:
+    out.writelines(lines_first)
     out.write(mini.minify(content))
+    out.writelines(lines_after)
